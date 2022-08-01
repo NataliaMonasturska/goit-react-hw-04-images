a1c5d8da22dbd649f2b289202f4510eb0cd3b5d6
Natalia Monasturska <tjomamon@gmail.com>
2022-08-01 14:33:10 +0300

    button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Button } from 'components/Button/Button';
 import css from './App.module.css';
 const axios = require('axios');
 
@@ -10,13 +11,15 @@ const MY_API_KEY = '27831514-d30de37ffbcb7c53880408e02';
 
 
 
-export class App extends Component  {
+export class App extends Component {
 
   state = {
     images: [],
     value: '',
-    page: 1
+    page: 1,
+    isLoading: false
   };
+
   async componentDidUpdate(_, prevState) {
     if (prevState.value !== this.state.value){
         try {
@@ -59,12 +62,49 @@ export class App extends Component  {
     this.setState({value, page: 1 })
   }
 
+  addsValueForPage = () => {
+    this.setState((prevState) => { page: prevState.page + 1 })
+  }
+
+  onClickLoadMore = async() => {
+  try {
+    const response = await axios.get(`https://pixabay.com/api/?key=${MY_API_KEY}&q=${this.state.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${this.state.page}`);
+    // console.log(response.data.hits);
+    this.setState({images: response.data.hits})
+    // if (response.data.hits.length === 0) {
+    //      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    // }
+    // let arr = response.data.hits;
+    // let lastPage = Math.ceil(response.data.totalHits / 40);
+    // totalHitsValue = response.data.totalHits;
+
+    // makeListCountries(arr);
+  
+    // if (response.data.total > 40) {
+    //   loadMore.classList.remove('visually-hidden');
+    // }
+    // if (pageforBtn === lastPage) {
+    //    if (!loadMore.classList.contains('visually-hidden')) {
+    //    loadMore.classList.add('visually-hidden')
+    //    }
+    // if (response.data.total <= 40) {
+    //     return
+    // }
+    //     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    // }
+  } catch (error) {
+    console.error(error);
+  }
+  }
+
 
   render () {
     return ( 
       <div className = {css.App}>
       <Searchbar onSubmit = {this.recordsValueInputForm} />
       <ImageGallery images={this.state.images}/>
+      {this.state.images.length > 0 && (<Button />)}
+  
       </div>
     );
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Button.module.css'
+
+export const Button = () => {
+    return (
+        <button type="button" class={css.Button} >Load more</button>
+    );
+  };
+
+
+
